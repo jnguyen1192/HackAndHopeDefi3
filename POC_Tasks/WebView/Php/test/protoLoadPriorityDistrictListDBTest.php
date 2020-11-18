ba87e5a63f44b687308bf49c19dc384aa7362597
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2020-11-18 19:34:43 +0100

    #7 Select points from DB done

--- POC_Tasks/WebView/Php/test/protoLoadPriorityDistrictListDBTest.php
+++ POC_Tasks/WebView/Php/test/protoLoadPriorityDistrictListDBTest.php
@@ -9,6 +9,8 @@ class protoLoadPriorityDistrictListDBTest extends TestCase
     public function testLoad_priority_district_list_db()
     {
         $pldd = new protoLoadPriorityDistrictListDB();// create object
+        $pldd->select_points_from_db();
+
         // retrieve data from db
         self::assertTrue(true);
     }
