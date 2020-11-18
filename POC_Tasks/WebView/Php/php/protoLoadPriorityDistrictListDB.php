56302c05f01ef614bf0dd7a2d7b673974c97f14e
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2020-11-18 20:00:24 +0100

    #7 Unit test select points from DB with first row done

--- POC_Tasks/WebView/Php/php/protoLoadPriorityDistrictListDB.php
+++ POC_Tasks/WebView/Php/php/protoLoadPriorityDistrictListDB.php
@@ -11,6 +11,7 @@ class protoLoadPriorityDistrictListDB
     {
         //$this->path_csv = $path_csv; // assign path on the object creation
         //$this->load_priority_district_list_db(); // load csv file with priority district list
+        $this->select_points_from_db();
     }
 
     // Methods
@@ -28,12 +29,14 @@ class protoLoadPriorityDistrictListDB
         {
 
             //$this->insert_PriorityDistrict($conn, $row); // insert into the database
-            $sql = 'SELECT nom_epci, ST_X(`geo_point_2d`) as latitude, ST_Y(`geo_point_2d`) as longitude FROM `qp` LIMIT 1'; // mysql query to select points from DB
+            $sql = 'SELECT nom_epci, ST_X(`geo_point_2d`) as latitude, ST_Y(`geo_point_2d`) as longitude FROM `qp`'; // mysql query to select points from DB
             $res = $conn->query($sql);
             if ($res->num_rows > 0) {
                 // output data of each row
                 while($row = $res->fetch_assoc()) {
-                    echo "nom_epci: " . $row["nom_epci"]. " - latitude longitude: " . $row["latitude"]. " " . $row["longitude"]. "<br>";
+                    $cols = array("nom_epci" => $row["nom_epci"], "latitude" => $row["latitude"], "longitude" => $row["longitude"]);
+                    //echo "nom_epci: " . $row["nom_epci"]. " - latitude longitude: " . $row["latitude"]. " " . $row["longitude"]. "<br>";
+                    array_push($this->list_row, $cols);
                 }
                 echo "Select awesome !!!"; // show a successfull message
                 return 0; // return 0 in case it works
--- POC_Tasks/WebView/Php/test/protoLoadPriorityDistrictListDBTest.php
+++ POC_Tasks/WebView/Php/test/protoLoadPriorityDistrictListDBTest.php
@@ -9,9 +9,11 @@ class protoLoadPriorityDistrictListDBTest extends TestCase
     public function testLoad_priority_district_list_db()
     {
         $pldd = new protoLoadPriorityDistrictListDB();// create object
-        $pldd->select_points_from_db();
-
-        // retrieve data from db
-        self::assertTrue(true);
+        $test_array = array("nom_epci" => "CA du Bassin de Bourg-en-Bresse", "latitude" => "5.24257553411", "longitude" => "46.2050735487");
+        //$list_row = select_points_from_db();
+        print_r($test_array);
+        print_r($pldd->list_row[0]);
+        // TODO retrieve data from db and test it
+        self::assertTrue($pldd->list_row[0]==$test_array);
     }
 }
