726ba6b1c5149966542777fe0440fa4c6a612d50
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2020-11-19 07:46:26 +0100

    #7 Unit test select marseille nursery school from DB

--- POC_Tasks/WebView/Php/mns.sql
+++ POC_Tasks/WebView/Php/mns.sql
@@ -2,3 +2,5 @@ CREATE TABLE `proto_qp`.`mns` (`Nom du site` VARCHAR(3000) NOT NULL , `Categorie
 
 
 --Nom du site;Categorie;Adresse 1;Adresse 2;Code Postal;Ville;Numero de telephone;Email;Longitude;Latitude
+--"Nom du site" => "Ecole Bergers", "Categorie" => "Ecoles maternelles", "Adresse 1" => "11 rue Perrin Solliers", "Adresse 2" => "", "Code Postal" => "13006", "Ville" => "Marseille", "Numero de telephone" => "04 91 47 63 62", "Email" => "ecole.mat.bergers@cime.org", "longitude" => "5.38485384024201", "latitude" => "43.2909458085538"
+--Ecole Bergers;Ecoles maternelles;11 rue Perrin Solliers;;13006;Marseille;04 91 47 63 62;ecole.mat.bergers@cime.org;5.38485384024201;43.2909458085538
---
+++ POC_Tasks/WebView/Php/test/protoLoadMarseilleNurserySchoolDBTest.php
@@ -0,0 +1,19 @@
+<?php
+
+require "../php/protoLoadMarseilleNurserySchoolDB.php";
+use PHPUnit\Framework\TestCase;
+
+class protoLoadMarseilleNurserySchoolDBTest extends TestCase
+{
+
+    public function testLoad_marseille_nursery_school_db()
+    {
+        $pmnsd = new protoLoadMarseilleNurserySchoolDB();// create object
+        $test_array = array("Nom du site" => "Ecole Bergers", "Categorie" => "Ecoles maternelles", "Adresse 1" => "11 rue Perrin Solliers", "Adresse 2" => "", "Code Postal" => "13006", "Ville" => "Marseille", "Numero de telephone" => "04 91 47 63 62", "Email" => "ecole.mat.bergers@cime.org", "longitude" => "5.38485384024201", "latitude" => "43.2909458085538");
+        //$list_row = select_points_from_db();
+        //print_r($test_array);
+        //print_r($pmnsd->list_row[0]);
+        // TODO retrieve data from db and test it
+        self::assertTrue($pmnsd->list_row[0]==$test_array);
+    }
+}
