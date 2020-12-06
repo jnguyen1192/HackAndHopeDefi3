78e3f28f1921cf305b5880bb15b96a69412bcbb9
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2020-12-06 20:23:47 +0100

    #7 Test geojson into mysql ready before coding

--- POC_Tasks/WebView/Php/test/protoLoadMarseilleSchoolExtensionGEOJSONTest.php
+++ POC_Tasks/WebView/Php/test/protoLoadMarseilleSchoolExtensionGEOJSONTest.php
@@ -10,37 +10,37 @@ class protoLoadMarseilleSchoolCSVTest extends TestCase
     public function testget_path_geojson()
     {
         // TODO
-        $path_csv = "C:\Users\johdu\PhpstormProjects\HackAndHopeDefi3\POC_Tasks\WebView\marseille_ecoles_maternelles_2018.csv"; // initialize the path
-        $lmnsCSV = new protoLoadMarseilleSchoolCSV($path_csv); // create a proto using path
-        echo $lmnsCSV->get_path_csv();
-        $this->assertTrue($lmnsCSV->get_path_csv() == $path_csv); // test if the path is correctly added into the proto
+        $path_geojson = "C:\Users\johdu\PhpstormProjects\HackAndHopeDefi3\POC_Tasks\WebView\marseille_ecoles_maternelles_2018.csv"; // initialize the path
+        $lmsGEOJSON = new protoLoadMarseilleSchoolExtensionGEOJSON($path_geojson); // create a proto using path
+        echo $lmsGEOJSON->get_path_csv();
+        $this->assertTrue($lmsGEOJSON->get_path_csv() == $path_geojson); // test if the path is correctly added into the proto
     }
 
     public function testLoad_marseille_annuaire_eduction_geojson()
     {
         // TODO
-        $path_csv = "C:\Users\johdu\PhpstormProjects\HackAndHopeDefi3\POC_Tasks\WebView\marseille_ecoles_maternelles_2018.csv"; // initialize the path
-        $lmnsCSV = new protoLoadMarseilleSchoolCSV($path_csv); // create a proto using path
+        $path_geojson = "C:\Users\johdu\PhpstormProjects\HackAndHopeDefi3\POC_Tasks\WebView\marseille_ecoles_maternelles_2018.csv"; // initialize the path
+        $lmsGEOJSON = new protoLoadMarseilleSchoolExtensionGEOJSON($path_csv); // create a proto using path
 
         //echo var_dump($lpdlCSV->list_row);
         //echo var_dump($lmnsCSV->list_row[0][0]);
         //echo var_dump($lmnsCSV->list_row[0][7]);
         //echo var_dump($lmnsCSV->list_row[0][8]);
         //echo var_dump($lmnsCSV->list_row[0][9]);
-        $this->assertTrue($lmnsCSV->list_row[0][0] == "Ecole Bergers"); // check code
-        $this->assertTrue($lmnsCSV->list_row[0][7] == "ecole.mat.bergers@cime.org"); // check point
-        $this->assertTrue($lmnsCSV->list_row[0][8] == "5.38485384024201"); // check point
-        $this->assertTrue($lmnsCSV->list_row[0][9] == "43.2909458085538"); // check point
+        $this->assertTrue($lmsGEOJSON->list_row[0][0] == "Ecole Bergers"); // check code
+        $this->assertTrue($lmsGEOJSON->list_row[0][7] == "ecole.mat.bergers@cime.org"); // check point
+        $this->assertTrue($lmsGEOJSON->list_row[0][8] == "5.38485384024201"); // check point
+        $this->assertTrue($lmsGEOJSON->list_row[0][9] == "43.2909458085538"); // check point
     }
 
     public function testinsert_annuaire_education_into_db()
     {
         // TODO
-        $path_csv = "C:\Users\johdu\PhpstormProjects\HackAndHopeDefi3\POC_Tasks\WebView\marseille_ecoles_maternelles_2018.csv"; // initialize the path
+        $path_geojson = "C:\Users\johdu\PhpstormProjects\HackAndHopeDefi3\POC_Tasks\WebView\marseille_ecoles_maternelles_2018.csv"; // initialize the path
 
-        $lpdlCSV = new protoLoadMarseilleSchoolCSV($path_csv, ","); // create a proto using the path
+        $lmsGEOJSON = new protoLoadMarseilleSchoolExtensionGEOJSON($path_geojson, ","); // create a proto using the path
 
-        $res = $lpdlCSV->insert_into_db(); // insert list row into the database
+        $res = $lmsGEOJSON->insert_into_db(); // insert list row into the database
 
         $this->assertTrue($res == 0);
     }
