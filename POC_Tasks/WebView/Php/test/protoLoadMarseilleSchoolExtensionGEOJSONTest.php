07a430694b63d3ca08fd90eb4a76b62ea7172d3c
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2020-12-10 23:53:52 +0100

    #7 Numero de telephone et effectif des élèves récupérés testé

--- POC_Tasks/WebView/Php/test/protoLoadMarseilleSchoolExtensionGEOJSONTest.php
+++ POC_Tasks/WebView/Php/test/protoLoadMarseilleSchoolExtensionGEOJSONTest.php
@@ -21,17 +21,12 @@ class protoLoadMarseilleSchoolExtensionGEOJSONTest extends TestCase
         // TODO
         $path_geojson = "C:/Users/johdu/PhpstormProjects/HackAndHopeDefi3/POC_Tasks/WebView/Php/data/geojson/fr-en-annuaire-education.geojson"; // initialize the path
         $lmsGEOJSON = new protoLoadMarseilleSchoolExtensionGEOJSON($path_geojson); // create a proto using path
-        echo var_dump($lmsGEOJSON->list_row['features'][20]["properties"]["telephone"]); # TODO use phone number to join (without space) with mns table number student
-        echo var_dump($lmsGEOJSON->list_row['features'][20]["properties"]["nombre_d_eleves"]);
-        //echo var_dump($lpdlCSV->list_row);
-        //echo var_dump($lmnsCSV->list_row[0][0]);
-        //echo var_dump($lmnsCSV->list_row[0][7]);
-        //echo var_dump($lmnsCSV->list_row[0][8]);
-        //echo var_dump($lmnsCSV->list_row[0][9]);
-        $this->assertTrue($lmsGEOJSON->list_row[0][0] == "Ecole Bergers"); // check code
-        $this->assertTrue($lmsGEOJSON->list_row[0][7] == "ecole.mat.bergers@cime.org"); // check point
-        $this->assertTrue($lmsGEOJSON->list_row[0][8] == "5.38485384024201"); // check point
-        $this->assertTrue($lmsGEOJSON->list_row[0][9] == "43.2909458085538"); // check point
+        //echo var_dump($lmsGEOJSON->list_row['features'][20]["properties"]["telephone"]); # TODO use phone number to join (without space) with mns table number student
+        //echo var_dump($lmsGEOJSON->list_row['features'][20]["properties"]["nombre_d_eleves"]);
+
+
+        $this->assertTrue($lmsGEOJSON->list_row['features'][20]["properties"]["telephone"] == "0491496079"); // check phone
+        $this->assertTrue($lmsGEOJSON->list_row['features'][20]["properties"]["nombre_d_eleves"] == 96); // check number
     }
 
     public function testinsert_annuaire_education_into_db()
