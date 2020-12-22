5adcd4f137d070aa358320c33e9832fc6ce6fdf4
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2020-12-22 22:01:32 +0100

    #7 Chargement d'un fichier Geojson OK, Présence du fichier Geojson OK, choix des propriétés à ajouter avec la colonne qui sera utilisée comme clé primaires

---
+++ POC_Tasks/WebView/Php/php/analyseGeojson.php
@@ -0,0 +1,19 @@
+<?php
+$uploaddir = '/var/www/uploads/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "Le fichier est valide, et a été téléchargé
+           avec succès. Voici plus d'informations :\n";
+} else {
+    echo "Attaque potentielle par téléchargement de fichiers.
+          Voici plus d'informations :\n";
+}
+
+echo 'Voici quelques informations de débogage :';
+print_r($_FILES);
+
+echo '</pre>';
+
+?>
