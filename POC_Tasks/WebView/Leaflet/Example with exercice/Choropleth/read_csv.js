cd79f9f7fb9de7c3b4c42698c629300194219340
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2021-02-26 07:50:20 +0100

    #7 Features updated, sources updated, JSON.parse used to convert csv file into javascript object

--- POC_Tasks/WebView/Leaflet/Example with exercice/Choropleth/read_csv.js
+++ POC_Tasks/WebView/Leaflet/Example with exercice/Choropleth/read_csv.js
@@ -1,4 +1,5 @@
 
+var statesData = "";
 $(document).ready(function() {
     $.ajax({
         type: "GET",
@@ -8,10 +9,17 @@ $(document).ready(function() {
     });
 });
 
+function pad(num, size) {
+    num = num.toString();
+    while (num.length < size) num = "0" + num;
+    return num;
+}
+
 function processData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(';');
     var lines = [];
+    var statesDataString = '{"type":"FeatureCollection","features":[';
 
     for (var i=1; i<allTextLines.length; i++) {
         var data = allTextLines[i].split(';');
@@ -33,7 +41,15 @@ function processData(allText) {
             newTemp = test.replace(/""/g, '"');
             var json_string = newTemp.slice(1, newTemp.length - 1)
             console.log(json_string);
+            statesDataString += '{"type":"Feature","id":"'+ pad(i, 6) +'","properties":{"name":"'+ lines[i][5] +'","density":94.65},"geometry":' + json_string + '},'
+            console.log();
             console.log(JSON.parse(json_string));
+
         }
     }
+    statesDataString += ']}';
+    statesData = JSON.parse(statesDataString);
+
+    console.log(statesData);
+    console.log("End");
 }
