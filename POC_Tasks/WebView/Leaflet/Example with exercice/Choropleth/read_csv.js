e8a233f5e952d3617a49cc39493e7e8ec3f5305e
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2021-02-27 07:15:52 +0100

    #7 Features updated, sources updated, read csv into json using ajax

--- POC_Tasks/WebView/Leaflet/Example with exercice/Choropleth/read_csv.js
+++ POC_Tasks/WebView/Leaflet/Example with exercice/Choropleth/read_csv.js
@@ -1,21 +1,20 @@
 
-var statesData = "";
-$(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "liste_quartiers_prioritairesville.csv",
-        dataType: "text",
-        success: function(data) {processData(data);}
-    });
-});
 
-function pad(num, size) {
-    num = num.toString();
-    while (num.length < size) num = "0" + num;
-    return num;
-}
+/*
+$(document).ready(function() {
+
+});*/
+
 
 function processData(allText) {
+
+    function pad(num, size) {
+        num = num.toString();
+        while (num.length < size) num = "0" + num;
+        return num;
+    }
+
+    //var file = loadFile("liste_quartiers_prioritairesville.csv");
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(';');
     var lines = [];
@@ -35,21 +34,149 @@ function processData(allText) {
     // alert(lines);
     for (var i=1; i<lines.length; i++) {
         var test = lines[i][11];
-        console.log(typeof(test));
-        console.log(lines[i]);
+        //console.log(typeof(test));
+        //console.log(lines[i]);
         if(test !== "") {
             newTemp = test.replace(/""/g, '"');
             var json_string = newTemp.slice(1, newTemp.length - 1)
-            console.log(json_string);
+            //console.log(json_string);
             statesDataString += '{"type":"Feature","id":"'+ pad(i, 6) +'","properties":{"name":"'+ lines[i][5] +'","density":94.65},"geometry":' + json_string + '},'
-            console.log();
-            console.log(JSON.parse(json_string));
+            //console.log();
+            //console.log(JSON.parse(json_string));
 
         }
     }
+    statesDataString = statesDataString.slice(0, -1);
     statesDataString += ']}';
-    statesData = JSON.parse(statesDataString);
+    //console.log(statesDataString);
+    //console.log(statesData);
+    console.log("Process file OK");
+    var statesData = JSON.parse(statesDataString);
+    var map = L.map('map').setView([43.32417965, 5.37450052], 14);
 
-    console.log(statesData);
-    console.log("End");
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/light-v9',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(map);
+
+
+    // control that shows state info on hover
+    var info = L.control();
+
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info');
+        this.update();
+        return this._div;
+    };
+
+    info.update = function (props) {
+        this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+            '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+            : 'Hover over a state');
+    };
+
+    info.addTo(map);
+
+
+    // get color depending on population density value
+    function getColor(d) {
+        return d > 1000 ? '#800026' :
+            d > 500  ? '#BD0026' :
+                d > 200  ? '#E31A1C' :
+                    d > 100  ? '#FC4E2A' :
+                        d > 50   ? '#FD8D3C' :
+                            d > 20   ? '#FEB24C' :
+                                d > 10   ? '#FED976' :
+                                    '#FFEDA0';
+    }
+
+    function style(feature) {
+        return {
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7,
+            fillColor: getColor(feature.properties.density)
+        };
+    }
+
+    function highlightFeature(e) {
+        var layer = e.target;
+
+        layer.setStyle({
+            weight: 5,
+            color: '#666',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+        }
+
+        info.update(layer.feature.properties);
+    }
+
+    var geojson;
+
+    function resetHighlight(e) {
+        geojson.resetStyle(e.target);
+        info.update();
+    }
+
+    function zoomToFeature(e) {
+        map.fitBounds(e.target.getBounds());
+    }
+
+    function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight,
+            click: zoomToFeature
+        });
+    }
+
+    geojson = L.geoJson(statesData, {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(map);
+
+    map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+
+
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+            labels = [],
+            from, to;
+
+        for (var i = 0; i < grades.length; i++) {
+            from = grades[i];
+            to = grades[i + 1];
+
+            labels.push(
+                '<i style="background:' + getColor(from + 1) + '"></i> ' +
+                from + (to ? '&ndash;' + to : '+'));
+        }
+
+        div.innerHTML = labels.join('<br>');
+        return div;
+    };
+
+    legend.addTo(map);
 }
+
+$.ajax({
+    type: "GET",
+    url: "liste_quartiers_prioritairesville.csv",
+    dataType: "text",
+    success: function(data) {statesData = processData(data);}
+});
