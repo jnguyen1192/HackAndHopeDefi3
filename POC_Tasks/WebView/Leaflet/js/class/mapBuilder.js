05766561b809425c1fa305421749b005bb4377cc
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2021-04-14 08:27:27 +0200

    #7 Features updated and cookie using location on last search added

--- POC_Tasks/WebView/Leaflet/js/class/mapBuilder.js
+++ POC_Tasks/WebView/Leaflet/js/class/mapBuilder.js
@@ -9,7 +9,15 @@ class MapBuilder {
   }
 
   create() {
-    let map = L.map(this.mapid).setView([43.28817965, 5.40450052], 11);//[43.32417965, 5.37450052], 14);
+    // chargement du cookie s'il existe
+    let map_location = [43.28817965, 5.40450052];
+    let map_zoom = 11;
+    if(getCookie("location") != "") {
+        //console.log(getCookie("location").split(",").map(Number));
+        map_location = getCookie("location").split(",").map(Number);
+        map_zoom = 18;
+    }
+    let map = L.map(this.mapid).setView(map_location, map_zoom);//[43.32417965, 5.37450052], 14);
     var controlLoading = L.Control.loading();
     map.addControl(controlLoading); // Ajout de l'icône de chargement
     L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
@@ -19,14 +27,18 @@ class MapBuilder {
 
     map.addLayer(this.markersLayer); // Ajout de la couche des marqueurs
 
-    map.addControl( new L.Control.Search({
-        container: this.searchbox,
+    new L.Control.Search({
+        //container: this.searchbox,
         layer: this.markersLayer,
         initial: false,
         collapsed: false,
         zoom: 14
         //tooltipLimit:-1
-    }) ); // Ajout de la boîte de recherche
+    }).on('search:locationfound', function(e) { // Ouverture de la popup après la recherche
+		e.layer.openPopup();
+		let object_cookie = [e.layer._latlng.lat, e.layer._latlng.lng] // e.layer; //
+		setCookie("location", object_cookie, 30);// enregistrement de la position dans les cookie
+	}).addTo(map); // Ajout de la boîte de recherche
 
     return map;
   }
