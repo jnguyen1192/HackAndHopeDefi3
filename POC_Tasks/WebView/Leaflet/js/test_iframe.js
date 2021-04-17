6d27b74de5e2513b96bfe1c01c154b55f6331ff6
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2021-04-17 07:31:36 +0200

    #7 Features, sources updated and navigation tab onclick display corrected on iframe

--- POC_Tasks/WebView/Leaflet/js/test_iframe.js
+++ POC_Tasks/WebView/Leaflet/js/test_iframe.js
@@ -6,3 +6,22 @@ window.onload = function() {
   link.type = "text/css"; 
   frames[0].document.head.appendChild(link); // 0 is an index of your iframe 
 }
+
+$(document).ready(function(){
+    $("#inlineFrameExample0").contents().find("li a").each(function() {
+        $(this).on('click', function() {
+            $("#inlineFrameExample0").contents().find("li a").each(function() {
+                $(this).parent().attr("class","");
+            });
+            $(this).parent().attr("class","active");
+        });
+
+    });
+    /*
+      $('.nav nav-tabs ul li a', window.parent.document).on('click', function() {
+        $('.nav nav-tabs ul li a', window.parent.document).attr("class","");
+        $(this).attr("class","active");
+        $($($(this).attr("id")).attr("class"), window.parent.document).attr("class","tab-pane");
+        $($(this).attr("id"), window.parent.document).attr("class", "tab-pane active in");
+      });*/
+});
