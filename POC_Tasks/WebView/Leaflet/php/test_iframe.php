ede94e881684d0560e880a5cf163f625cc7fdcd4
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2021-04-20 07:51:10 +0200

    #7 Features, sources updated, cookie html added, div selected on iframe

--- POC_Tasks/WebView/Leaflet/php/test_iframe.php
+++ POC_Tasks/WebView/Leaflet/php/test_iframe.php
@@ -1,1 +1,22 @@
-<?php echo file_get_contents($_GET['url']); ?>
+<?php
+
+$content = file_get_contents($_GET['url']);
+libxml_use_internal_errors(true);
+$doc = new DOMDocument();
+$doc->loadHtml($content);
+
+$xpath = new DOMXpath($doc);
+libxml_clear_errors();
+$elements = $xpath->query('//*[@id="pgui-view-grid"]/div[count(preceding-sibling::*)=1]/div[count(preceding-sibling::*)=1]');
+
+$domElemsToRemove = ''; // container of deleted elements
+foreach ( $elements as $domElement ) {
+    $domElemsToRemove .= $doc->saveHTML($domElement); // concat them
+    $domElement->parentNode->removeChild($domElement); // then remove
+}
+
+//$form_div = explode("\n", trim(document.querySelector("#pgui-view-grid > div:nth-child(2)")))[0];
+
+echo $domElemsToRemove;
+
+ ?>
