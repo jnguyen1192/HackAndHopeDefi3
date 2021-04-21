60526aa7c47be7306a2d631a77013121ee5c8b3d
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2021-04-21 07:56:25 +0200

    #7 Features updated, tab from phpgenerator behavior on iframe corrected

--- POC_Tasks/WebView/Leaflet/js/test_iframe.js
+++ POC_Tasks/WebView/Leaflet/js/test_iframe.js
@@ -1,27 +1,44 @@
 
 window.onload = function() {
-  let link = document.createElement("link");
-  link.href = "../css/test_iframe.css";     
-  link.rel = "stylesheet"; 
-  link.type = "text/css"; 
-  frames[0].document.head.appendChild(link); // 0 is an index of your iframe 
-}
+    let link = document.createElement("link");
+    link.href = "../css/test_iframe.css";
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    frames[0].document.head.appendChild(link); // 0 is an index of your iframe
 
-$(document).ready(function(){
-    $("#inlineFrameExample0").contents().find("li a").each(function() {
-        $(this).on('click', function() {
-            $("#inlineFrameExample0").contents().find("li a").each(function() {
-                $(this).parent().attr("class","");
+
+    $(document).ready(function(){
+        // tab link behavior
+        $("#inlineFrameExample0").contents().find("li a").each(function() {
+            $(this).on('click', function() {
+                $("#inlineFrameExample0").contents().find("li a").each(function() {
+                    $(this).parent().attr("class","");
+                });
+                $(this).parent().attr("class","active");
+                $("#inlineFrameExample0").contents().find(".tab-pane").each(function() {
+                    $(this).attr("class","tab-pane");
+                });
+                //console.log($(this).attr("href"));
+                var id = $(this).attr("href");
+                $("#inlineFrameExample0").contents().find(".tab-pane").each(function() {
+                    //console.log($(this).attr("id"));
+                    if(id == "#" + $(this).attr("id")) {
+                        $(this).attr("class","tab-pane active in");
+                    }
+                });
+                //$("div"+$(this).attr("href")).setAttribute("class", "tab-pane active in");
+
+
             });
-            $(this).parent().attr("class","active");
-        });
 
+        });
+        // tab content behavior
+        /*
+          $('.nav nav-tabs ul li a', window.parent.document).on('click', function() {
+            $('.nav nav-tabs ul li a', window.parent.document).attr("class","");
+            $(this).attr("class","active");
+            $($($(this).attr("id")).attr("class"), window.parent.document).attr("class","tab-pane");
+            $($(this).attr("id"), window.parent.document).attr("class", "tab-pane active in");
+          });*/
     });
-    /*
-      $('.nav nav-tabs ul li a', window.parent.document).on('click', function() {
-        $('.nav nav-tabs ul li a', window.parent.document).attr("class","");
-        $(this).attr("class","active");
-        $($($(this).attr("id")).attr("class"), window.parent.document).attr("class","tab-pane");
-        $($(this).attr("id"), window.parent.document).attr("class", "tab-pane active in");
-      });*/
-});
+}
--- POC_Tasks/WebView/Leaflet/php/test_iframe.php
+++ POC_Tasks/WebView/Leaflet/php/test_iframe.php
@@ -1,13 +1,13 @@
 <?php
 
 $content = file_get_contents($_GET['url']);
-libxml_use_internal_errors(true);
+libxml_use_internal_errors(true); // hide warning
 $doc = new DOMDocument();
 $doc->loadHtml($content);
 
 $xpath = new DOMXpath($doc);
-libxml_clear_errors();
-$elements = $xpath->query('//*[@id="pgui-view-grid"]/div[count(preceding-sibling::*)=1]/div[count(preceding-sibling::*)=1]');
+libxml_clear_errors(); // hide warning
+$elements = $xpath->query('//*[@id="pgui-view-grid"]/div[count(preceding-sibling::*)=1]/div[count(preceding-sibling::*)=1]'); // https://css-selector-to-xpath.appspot.com/
 
 $domElemsToRemove = ''; // container of deleted elements
 foreach ( $elements as $domElement ) {
