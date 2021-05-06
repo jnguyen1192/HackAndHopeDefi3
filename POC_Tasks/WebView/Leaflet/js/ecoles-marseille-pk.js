cc24ac45b7cb25dd93a5b20752b62c512b0837ce
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2021-05-06 08:04:05 +0200

    #7 Features updated and refactor code on iframe

--- POC_Tasks/WebView/Leaflet/js/ecoles-marseille-pk.js
+++ POC_Tasks/WebView/Leaflet/js/ecoles-marseille-pk.js
@@ -89,8 +89,18 @@ function processJsonData(jsonData, markersLayer, nb_marker=null) {
             //console.log(point[1].pk)
             // TODO use a file called popup.html to get the code and customize it
             // TODO https://dev.nos-ecoles.fr/demande_intervention.php?operation=insert
-            popup = '<a class="leaflet-popup-previous-button" title="Retour" href="#" onclick="previousIframe()">&lt;</a><iframe id="inlineFrameExample0" class="iframes" style="padding-top: 2%; width: 100%; height: 98%;border-width: 0px;" title="Inline Frame Example0" src="./php/test_iframe.php?url=https%3A%2F%2Fdev.nos-ecoles.fr%2Fnos_ecoles.php%3Foperation%3Dview%26pk0%3D' + point[1].pk.toString() + '"></iframe>';
+            /* const fileUrl = '' // provide file location
 
+fetch(fileUrl)
+   .then( r => r.text() )
+   .then( t => console.log(t) )
+   */
+            //console.log(getPopupHTML());
+
+            //popup = '<a class="leaflet-popup-previous-button" title="Retour" href="#" onclick="previousIframe()">&lt;</a><iframe id="inlineFrameExample0" class="iframes" style="padding-top: 2%; width: 100%; height: 98%;border-width: 0px;" title="Inline Frame Example0" src="./php/test_iframe.php?url=https%3A%2F%2Fdev.nos-ecoles.fr%2Fnos_ecoles.php%3Foperation%3Dview%26pk0%3D' + point[1].pk.toString() + '"></iframe>';
+            var link = "https://dev.nos-ecoles.fr/nos_ecoles.php?operation=view&pk0="; // TODO get link from a config file
+            popup = getPopupHTML().replace("{ecole_pk}",point[1].pk.toString()).replace("{link}", encodeURIComponent(link));
+            console.log(popup);
             //console.log(popup)
             /* 4) Marker with popup added on map */
             var marker = L.marker(new L.latLng([point[1].ecole_lat, point[1].ecole_long]), config)
@@ -119,6 +129,25 @@ function getJsonFromDB() {
 
     return donnees;
 }
+function getPopupHTML() {
+    var donnees;
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = () => {
+        // La transaction est terminée ?
+        if(xmlhttp.readyState == 4){
+            // Si la transaction est un succès
+            if(xmlhttp.status == 200){
+                // On traite les données reçues
+                donnees = xmlhttp.responseText
+            }
+        }
+    }
+    xmlhttp.open("GET", "popup.html", false); // False pour avoir un comportement synchrone
+    xmlhttp.send(null);
+
+    return donnees;
+}
+
 
 function previousIframe() {
     $("#inlineFrameExample0").attr('src', window.history.back());
