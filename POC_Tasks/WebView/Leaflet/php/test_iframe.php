6282b2c675152089fd65872fe7a942271a36c580
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2021-05-28 07:42:50 +0200

    #7 Features updated and parameter on URL added on demande amelioration form

--- POC_Tasks/WebView/Leaflet/php/test_iframe.php
+++ POC_Tasks/WebView/Leaflet/php/test_iframe.php
@@ -17,9 +17,21 @@ foreach ( $elements as $domElement ) {
 
 //$form_div = explode("\n", trim(document.querySelector("#pgui-view-grid > div:nth-child(2)")))[0];
 echo '<link href="../css/test_iframe.css" rel="stylesheet" type="text/css">';
+echo '<a class="leaflet-popup-demande-amelioration-button" title="Demander une amélioration" href="https://dev.nos-ecoles.fr/demande_amelioration.php?operation=insert" onclick="demandeAmelioration()">Demander une amélioration</a>
+<a target="_blank" class="leaflet-popup-demande-intervention-button" title="Demander une intervention" href="https://dev.nos-ecoles.fr/demande_intervention.php?operation=insert" onclick="demandeIntervention()">Demander une intervention</a>
+<a target="_blank" class="leaflet-popup-demande-sst-button" title="Demander une fiche SST" href="https://dev.nos-ecoles.fr/demande_sst.php?operation=insert" onclick="demandeSST()">Demander une fiche SST</a>
+';
 echo $domElemsToRemove;
+
+//
+/*
+Add
+document.getElementById("select2-chosen-12").innerHTML = {ecole_id};
+
+*/
 echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script>
+$(".leaflet-popup-demande-amelioration-button").attr("href","http://google.fr");
 //console.log($(".row"));
 $(".row").contents().find("li a").each(function() {
             $(this).on("click", function() {
